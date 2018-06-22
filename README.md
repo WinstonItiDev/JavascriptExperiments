cdf8ca620a3f2566b83e19f4788adcab116fad8e
WinstonItiDev <winstonshred@gmail.com>
2018-06-22 16:20:37 +1200

    committed javascript test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Javascript_Experiments
+
+- For the study of programming fundamentals and concepts
+
+22 06 2018
---
+++ js/javascript-basics.js
@@ -0,0 +1,52 @@
+
+var renderer;
+var graphics;
+
+var widthOfScreen = 800;
+var heightOfScreen = 80;
+
+var circles = [];
+
+function initRenderer() {
+
+	console.log("initializing renderer..");
+
+	renderer = new PIXI.Application(widthOfScreen, heightOfScreen, { antialias: true });
+	document.getElementById("pixidisplay").appendChild(renderer.view);
+
+	graphics = new PIXI.Graphics();
+
+	for (var i = 0; i < 100; i++) {
+		circles[i] = new CircleShape(Math.floor((Math.random() * widthOfScreen) + 1), Math.floor((Math.random() * heightOfScreen) + 1), Math.floor((Math.random() * 10) + 1));
+	}
+	
+}
+
+function draw() {
+	console.log("drawing..");
+
+	for (var i = 0; i < 100; i++) {
+		circles[i].drawCircleShape();
+	}
+
+
+}
+
+function CircleShape(_x, _y, _s) {
+
+	this.x = _x;
+	this.y = _y;
+	this.s = _s;
+
+	this.drawCircleShape = function drawCircleShape() {
+
+	graphics.lineStyle(0);
+	graphics.beginFill(0xFFFF0B, 0.5);
+	graphics.drawCircle(this.x, this.y,this.s);
+	graphics.endFill();
+
+	renderer.stage.addChild(graphics);
+
+	}
+}
+
